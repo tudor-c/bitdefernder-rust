08feeb697ba0ed85e2e2a82b4e6e2337bcd77167
tudor-c <tudor.copilu@gmail.com>
2024-04-11 19:09:04 +0300

    File listing

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,52 +1,52 @@
-use std::env;
-use std::io::prelude::*;
-use std::fs::File;
-use std::io::Read;
+use std::{
+    ffi::OsStr,
+    fs::File,
+    io::{Read, Seek}, vec,
+};
 
-struct Archive {
+struct FileData {
     name: String,
-    files: Vec<String>,
-    file_count: u32
+    filenames: Vec<String>
 }
 
-fn list_zip_contents(reader: impl Read + Seek) -> zip::result::ZipResult<()> {
-    let mut zip = zip::ZipArchive::new(reader);
+fn get_zip_contents(reader: impl Read + Seek) -> Vec<String> {
+    let mut zip = zip::ZipArchive::new(reader).unwrap();
+    let mut filenames: Vec<String> = Vec::new();
 
     for i in 0..zip.len() {
-        let file = zip.by_index(i)?;
-        println!("Filename: {}", file.name());
+        let file = zip.by_index(i).unwrap();
+        filenames.push(file.name().to_string());
     }
 
-    Ok(())
+    return filenames;
 }
 
-fn create_zip_struct(zip_file: impl Read + Seek) -> Option<Archive, ()> {
-    let mut zip = zip::ZipArchive::new(zip_file);
-    match zip {
-        Err(e) => None,
-        Ok(v) => {
-            let zip_archive = zip.unwrap();
-            let files = Vec::new();
-            for i in 0...&zip_archive.len() {
-                files.push(zip_archive.by_index(i))
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = std::env::args().collect();
+    let mut data: Vec<FileData> = Vec::new();
+
+    let dir = &args[1];
+    for entry in std::fs::read_dir(dir)? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_file() && path.extension() == Some(OsStr::new("zip")) {
+            let file = File::open(&path)?;
+            let zip_name = path.file_name().unwrap().to_string_lossy().to_string();
+            let filenames = get_zip_contents(file);
+
+            for filename in filenames.iter() {
+                println!("File in {}: {}", zip_name, filename);
             }
-            return Some(Archive {
-                name: &zip_file,
-                files: ,
-                file_count: zip_archive.len()
-            })
+
+            data.push(FileData {
+                name: zip_name,
+                filenames: filenames
+            });
+
+        } else {
+            println!("Skipping {:?}", path);
         }
-
     }
-    // Ok(Archive { name: "name".to_string(), files: Vec::new(), file_count: 0})
 
-}
-
-fn main() -> std::io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    let path = &args[1];
-    let f = File::open(&path)?;
-
-    list_zip_contents(&f)?;
     Ok(())
 }
