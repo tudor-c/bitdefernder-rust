c3f3c5af72ba149c292f8ff05bc9ed51e96e6372
tudor-c <tudor.copilu@gmail.com>
2024-05-16 20:17:28 +0300

    /search request

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,150 +1,199 @@
+#[macro_use]
+extern crate rocket;
+
 use std::{
-    collections::{HashMap, HashSet}, fs::File, hash::Hash, io::{BufRead, BufReader}, process::exit, time::Instant, vec
+    collections::HashMap,
+    fs::File,
+    io::{BufRead, BufReader},
+    sync::{Arc, RwLock},
+    time::Instant,
 };
 
-use serde::{Deserialize, Serialize};
-use serde_json::value::Index;
+use rocket::{fs::FileServer, serde::json::Json, State};
 
-#[derive(Debug, Serialize, Deserialize)]
-struct FileData {
-    name: String,
-    files: Vec<String>,
-}
+use serde::{Deserialize, Serialize};
 
 type Term = String;
 type DocumentId = String;
-type IndexType = HashMap<Term, HashSet<DocumentId>>;
 
+#[derive(Default)]
 struct IndexedData {
-    terms_to_docs: HashMap<Term, HashSet<DocumentId>>,
+    terms_to_docs: HashMap<Term, Vec<DocumentId>>,
     idf: HashMap<Term, f64>,
+    num_docs: usize,
 }
 
 impl IndexedData {
     pub fn new() -> Self {
-        Self {
-            terms_to_docs: HashMap::new(),
-            idf: HashMap::new(),
-        }
+        Self::default()
     }
 }
 
+#[derive(Serialize)]
+struct Greeting {
+    message: String,
+}
 
-fn read_data(data_filename: &str) -> Result<Vec<FileData>, Box<dyn std::error::Error>> {
+#[derive(Deserialize)]
+struct SearchData {
+    terms: Vec<String>,
+    min_score: Option<i32>,
+    max_length: Option<f64>,
+}
+
+#[derive(Serialize)]
+struct SearchResult {
+    total: usize,
+    matches: Vec<SearchMatch>
+}
+
+#[derive(Serialize)]
+struct SearchMatch {
+    md5: DocumentId,
+    score: f64
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct FileData {
+    /// name of the zip archive
+    name: DocumentId,
+    /// list of files in the zip archive
+    files: Vec<String>,
+}
+
+fn compute_idf(terms_to_docs: &HashMap<Term, Vec<DocumentId>>) -> HashMap<Term, f64> {
+    let n = terms_to_docs.len() as f64;
+    let mut terms_idf = HashMap::new();
+    for (term, docs) in terms_to_docs {
+        let nq = docs.len() as f64;
+        let idf = ((n - nq + 0.5) / (nq + 0.5)).ln();
+        terms_idf.insert(term.clone(), idf);
+    }
+
+    terms_idf
+}
+
+fn load_data(data_filename: &str, limit: Option<usize>) -> eyre::Result<IndexedData> {
     let file = File::open(data_filename)?;
     let reader = BufReader::new(file);
 
-    let mut data: Vec<FileData> = Vec::new();
-    for line in reader.lines().take(100) {
+    let mut index = IndexedData::new();
+    for line in reader.lines().take(limit.unwrap_or(usize::MAX)) {
         let line = line?;
-        let line = line.trim();
-        data.push(serde_json::from_str(line)?);
-    }
-    Ok(data)
-}
 
-fn load_data(data: &Vec<FileData>) -> Result<IndexType, Box<dyn std::error::Error>> {
-    let mut index = IndexType::new();
-
-    for filedata in data {
-        let name = &filedata.name;
-        let files = &filedata.files;
-        for item in files {
-            let tokens = item.split('/');
-            for token in tokens {
-                index.entry(token.to_string())
-                    .or_insert(HashSet::new())
-                    .insert(name.to_string());
+        let fd: FileData = serde_json::from_str(&line)?;
+        for file in fd.files {
+            for term in file.split("/") {
+                if let Some(set) = index.terms_to_docs.get_mut(term) {
+                    if set.last() != Some(&fd.name) {
+                        set.push(fd.name.clone());
+                    }
+                } else {
+                    let mut set = Vec::new();
+                    set.push(fd.name.clone());
+                    index.terms_to_docs.insert(term.to_string(), set);
+                }
             }
         }
+
+        index.num_docs += 1;
     }
+
+    index.idf = compute_idf(&index.terms_to_docs);
     Ok(index)
 }
 
-fn compute_idf(count: u64, n: &HashMap<Term, u64>) -> Result<HashMap<Term, f64>, Box<dyn std::error::Error>> {
-    let mut idf = HashMap::new();
-    for term in n.keys() {
-        let val = (count as f64 - n[term] as f64 + 0.5) / (n[term] as f64 + 0.5) + 1.0;
-        idf.insert(term.to_string(), val.ln());
-    }
-    Ok(idf)
-}
-
-fn compute_freq(data: &Vec<FileData>) -> Result<HashMap<(Term, DocumentId), u64>, Box<dyn std::error::Error>> {
-    let mut res = HashMap::new();
-    for file_data in data {
-        for term in &file_data.files {
-            let total = res.entry((term.to_string(), file_data.name.to_string())).or_insert(0u64);
-            *total += 1
-        }
-    }
-    Ok(res)
-}
-
-fn compute_len(data: &Vec<FileData>) -> Result<HashMap<DocumentId, u64>, Box<dyn std::error::Error>> {
-    let mut res = HashMap::new();
-    for file_data in data {
-        res.insert(file_data.name.to_string(), file_data.files.len() as u64);
-    }
-    Ok(res)
-}
-
-fn run_search(data: &IndexType, terms: Vec<&str>) -> HashMap<DocumentId, u64> {
+fn run_search(data: &IndexedData, terms: Vec<&str>) -> SearchResult {
     let mut counter: HashMap<DocumentId, u64> = HashMap::new();
-
-    for term in terms {
-        for name in data.get(term).unwrap() {
-            let total = counter.entry(name.to_string()).or_insert(0);
-            *total += 1;
+    for term in &terms {
+        if let Some(docs) = data.terms_to_docs.get(*term) {
+            for doc in docs {
+                let x = counter.entry(doc.to_string()).or_insert(0);
+                *x += 1;
+            }
         }
     }
-    counter
+
+    let mut matches: Vec<SearchMatch> = Vec::new();
+    for (doc, cnt) in counter {
+        matches.push(SearchMatch {
+                md5: doc.to_string(),
+                score: cnt as f64 / terms.len() as f64
+            }
+        );
+    }
+    matches.sort_by(|a, b| b.score.total_cmp(&a.score));
+    SearchResult {
+        total: matches.len(),
+        matches: matches,
+    }
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[get("/")]
+fn index() -> Json<Greeting> {
+    Json(Greeting {
+        message: "Hello, welcome to our server!".to_string(),
+    })
+}
+
+#[post("/search", data = "<req>")]
+fn search(req: Json<SearchData>, server_state: &State<Arc<RwLock<ServerState>>>) -> Result<Json<SearchResult>, String> {
+    let index = &server_state.read().map_err(|err| format!("Err: {:#}", err))?.index;
+    let terms = req.terms.iter().map(|s| s.as_str()).collect();
+    let result = run_search(&index, terms);
+    Ok(Json(result))
+}
+
+#[derive(Default)]
+struct ServerState {
+    index: IndexedData,
+}
+
+#[rocket::main]
+async fn main() -> eyre::Result<()> {
     let args: Vec<String> = std::env::args().collect();
     let data_filename = &args[1];
-    let data = read_data(data_filename)?;
-    let index = load_data(&data)?;
+    let limit = args
+        .get(2)
+        .map(|x| usize::from_str_radix(&x, 10))
+        .transpose()?;
 
-    let mut n_val = HashMap::new();
-    for (key, _) in &index {
-        n_val.insert(key.to_string(), index.get(key).unwrap().len() as u64);
-    }
+    println!("loading {data_filename}...");
+    let start = Instant::now();
 
-    let indexedData = IndexedData {
-        terms_to_docs: index,
-        idf: compute_idf(data.len() as u64, &n_val)?
-    };
-    let term_freq = compute_freq(&data)?;
-    let doc_len = compute_len(&data)?;
-    let avgdl = doc_len.values().sum::<u64>() / doc_len.len() as u64;
-    let k1 = 1.2;
-    let b = 0.75;
+    let data = load_data(data_filename, limit)?;
 
-    let score: HashMap<DocumentId, f64> = HashMap::new();
-    for (name, terms) in indexedData.terms_to_docs {
-        let mut score = 0.0;
-        for term in terms {
-            score += idf.get(term) * (term_freq.get((term, name)))
-        }
-    }
+    let pair_count = data
+        .terms_to_docs
+        .iter()
+        .map(|(_, docs)| docs.len())
+        .sum::<usize>();
+    println!(
+        "loaded data for {} docs, {} terms, {} term-docid pairs, in {:.2}s",
+        data.num_docs,
+        data.terms_to_docs.len(),
+        pair_count,
+        start.elapsed().as_secs_f64(),
+    );
 
-    // let mut total = 0;
-    // for (_, names) in &indexedData.terms_to_docs {
-    //     total += names.len();
-    // }
-    // println!("terms: {}\npairs: {}", &indexedData.terms_to_docs.keys().len(), &total);
+    let start = Instant::now();
+    let search = vec!["lombok", "AUTHORS", "README.md"];
+    let matches = run_search(&data, search);
+    println!(
+        "search found {} matches in {:.2}s",
+        matches.matches.len(),
+        start.elapsed().as_secs_f64(),
+    );
 
-    let search_timer = Instant::now();
-    let _result = run_search(&indexedData.terms_to_docs, ["cat.jpg", "DebugProbesKt.bin", "phonenumbers"].to_vec());
-    // for (name, count) in result {
-    //         println!("name = {}, count = {}", &name, &count);
-    // }
-    println!("search took: {:?}", &search_timer.elapsed());
+    let server_state = Arc::new(RwLock::new(ServerState { index: data }));
+    rocket::build()
+        .manage(server_state)
+        .mount("/", routes![index, search])
+        .mount("/dashboard", FileServer::from("static"))
+        .ignite()
+        .await?
+        .launch()
+        .await?;
 
-    for val in &indexedData.idf {
-        println!("{:?}", &val);
-    }
     Ok(())
 }
